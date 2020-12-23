e5523c5e5bfc14beaead88bc0d05ccabfa1cad93
0xTetra <0xtetra420@protonmail.com>
2020-12-23 19:23:20 +0000

    help test

--- commands/info/help.js
+++ commands/info/help.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'Displays a list of bot-related commands.',
     category: 'info',
     run: async (client, message, args) => {
-        await message.channel.send(client.commands);
+        console.log(client.commands);
     }
 }
