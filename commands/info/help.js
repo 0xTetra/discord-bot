701c9debe095bb274a657fcc597d73bac81197ac
0xTetra <0xtetra420@protonmail.com>
2020-12-23 20:27:33 +0000

    help

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,17 +1,26 @@
 const fs = require('fs');
+const Discord = require('discord.js');
 
 module.exports = {
     name: 'help',
     description: 'Displays a list of bot-related commands.',
     category: 'info',
     run: async (client, message, args) => {
-        fs.readdirSync('/app/commands/').forEach(cat => {
+        let commandList = {};
+        let descriptionList = {};
+        await fs.readdirSync('/app/commands/').forEach(cat => {
             const commands = fs.readdirSync(`/app/commands/${cat}/`).filter(cmd => cmd.endsWith('.js'));
         
             for (const command of commands) {
                 const com = require(`/app/commands/${cat}/${command}`);
-                message.channel.send(com.name);
+                commandList.push(com.name)
+                descriptionList.push(com.description);
             }
         });
+
+        const helpEmbed = new Discord.MessageEmbed()
+            .setColor('#80ff33')
+            .setTitle('Command List')
+            .addField(`${commandList}, ${descriptionList}`) // I kinda forgot what you wanted me to do
     }
 }
