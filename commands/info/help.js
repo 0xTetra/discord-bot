786fee5a7b55d5156965ff306d9f371c13dfd10b
0xTetra <0xtetra420@protonmail.com>
2020-12-23 19:32:03 +0000

    help test

--- commands/info/help.js
+++ commands/info/help.js
@@ -3,6 +3,13 @@ module.exports = {
     description: 'Displays a list of bot-related commands.',
     category: 'info',
     run: async (client, message, args) => {
-        console.log(client.commands);
+        fs.readdirSync('/app/commands/').forEach(cat => {
+            const commands = fs.readdirSync(`/app/commands/${cat}/`).filter(cmd => cmd.endsWith('.js'));
+        
+            for (const command of commands) {
+                const com = require(`/app/commands/${cat}/${command}`);
+                message.channel.reply(com.name);
+            }
+        });
     }
 }
