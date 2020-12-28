4204fb73d462e8c152952e1d791b9e430e7f1965
0xTetra <0xtetra420@protonmail.com>
2020-12-28 16:15:48 +0000

    meme command

---
+++ commands/fun/meme.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: 'meme',
+    description: 'Generates a random meme from a subreddit.',
+    category: 'meme',
+    run: async (client, message, args) => {
+        await axios.get('https://meme-api.herokuapp.com/gimme').then(meme => {
+            const memeEmbed = new Discord.MessageEmbed()
+                .setColor('#80ff33')
+                .setTitle(meme.body.subreddit)
+                .setImage(meme.body.url)
+
+            message.channel.send(memeEmbed);
+        });
+    }
+}
