b76e2452d1a10abe91a52940a6abc4ee8f1ada45
0xTetra <0xtetra420@protonmail.com>
2020-12-28 19:55:44 +0000

    joke shit

---
+++ commands/fun/joke.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const axios = require('axios');
+
+module.exports = {
+    name: 'joke',
+    description: 'Generates a random joke from some retarded api',
+    category: 'fun',
+    run: async (client, message, args) => {
+        await axios.get('https://v2.jokeapi.dev/joke/Dark?type=twopart').then(joke => {
+            const jokeEmbed = new Discord.MessageEmbed()
+                .setColor('#80ff33')
+                .setTitle(joke.data.setup)
+                .setDescription(joke.data.delivery)
+
+            message.channel.send(jokeEmbed);
+        });
+    }
+}
