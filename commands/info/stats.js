6a51c8114edeefccb7df86343929d803ea88299c
0xTetra <0xtetra420@protonmail.com>
2020-12-28 17:05:21 +0000

    added uptime to stats

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -10,12 +10,24 @@ module.exports = {
         const totalUsers = client.guilds.cache.reduce((a, g) => a + g.memberCount, 0)
         const totalServers = client.guilds.cache.size;
 
+        const getUptime = () => {
+            const pad = (s) => {
+                return (s < 10 ? '0' : '') + s;
+            }
+            var hours = Math.floor(seconds / (60*60));
+            var minutes = Math.floor(seconds % (60*60) / 60);
+            var seconds = Math.floor(seconds % 60);
+            
+            return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+        }
+
         const statsEmbed = new Discord.MessageEmbed()
             .setColor('#80ff33')
             .setTitle('Statistics')
-            .addField('Total Users: ', totalUsers, true)
-            .addField('Total Servers: ', totalServers, true)
-            .addField('Node Version: ', process.version, false)
+            .addField('Total Users:', totalUsers, true)
+            .addField('Total Servers:', totalServers, true)
+            .addField('Node Version:', process.version, true)
+            .addField('Uptime:', getUptime(), false)
 
         await message.channel.send(statsEmbed);
     }
