7bbb1d2d756e6864a8cfbbea7dbe20b12f834174
0xTetra <0xtetra420@protonmail.com>
2020-12-29 13:04:15 +0000

    help improvements

--- commands/info/help.js
+++ commands/info/help.js
@@ -6,51 +6,43 @@ module.exports = {
     description: 'Displays a list of bot-related commands.',
     category: 'info',
     run: async (client, message, args) => {
+        let categoryList = ['info', 'fun'];
+
+        const capitalize = (s) => {
+            if (typeof s !== 'string') return ''
+            return s.charAt(0).toUpperCase() + s.slice(1)
+        }
+
         if (args.length == 0) {
-            const categoryList = [];
             const helpEmbed = new Discord.MessageEmbed()
                 .setColor('#80ff33')
                 .setTitle('Category List')
 
-
-            await fs.readdirSync('/app/commands/').forEach(cat => {
-                    categoryList.push(cat);
-            });
             helpEmbed.setDescription(categoryList.join('\n'));
             await message.channel.send(helpEmbed);
-        } else if (args[0] == 'fun') {
-            const funEmbed = new Discord.MessageEmbed()
-                .setColor('80ff33')
-                .setTitle('Fun Command List')
-
-            const commands = await fs.readdirSync('/app/commands/fun/').filter(file => file.endsWith('.js'));
-
-            for (const cmd of commands) {
-                const command = require(`/app/commands/fun/${cmd}`);
-                funEmbed.addField(`${client.prefix}${command.name}`, command.description);
-            }
-
-            await message.channel.send(funEmbed);
-        } else if (args[0] == 'info') {
-            const infoEmbed = new Discord.MessageEmbed()
-                .setColor('#80ff33')
-                .setTitle('Info Command List')
-
-            const commands = await fs.readdirSync('/app/commands/info/').filter(file => file.endsWith('.js'));
-
-            for (const cmd of commands) {
-                const command = require(`/app/commands/info/${cmd}`);
-                infoEmbed.addField(`${client.prefix}${command.name}`, command.description);
-            }
-
-            await message.channel.send(infoEmbed);
         } else {
-            const invalidSyntax = new Discord.MessageEmbed()
+            if (categoryList.indexOf(args[0]) > -1) {
+                const capitalized = capitalize(args[0]);
+                const catEmbed = new Discord.MessageEmbed()
+                .setColor('#80ff33')
+                .setTitle(`${capitalized} Command List`)
+
+                const commands = await fs.readdirSync(`/app/commands/${args[0]}/`).filter(file => file.endsWith('.js'));
+
+                for (const cmd of commands) {
+                    const command = require(`/app/commands/info/${cmd}`);
+                    infoEmbed.addField(`${client.prefix}${command.name}`, command.description);
+                }
+
+                await message.channel.send(catEmbed);
+            } else {
+                const invalidSyntax = new Discord.MessageEmbed()
                 .setColor('#ff0000')
                 .setTitle('Invalid Syntax')
                 .setDescription(`Usage: ${client.prefix}help [Category]`)
 
-            await message.channel.send(invalidSyntax);
+                await message.channel.send(invalidSyntax);
+            }
         }
             
     }
