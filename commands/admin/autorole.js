da012f105a670e09fc6351652b63ddcc2ae4fc27
Tetra <0xtetra420@protonmail.com>
2021-01-04 15:57:14 +0000

    autorole testing

--- commands/admin/autorole.js
+++ commands/admin/autorole.js
@@ -20,7 +20,7 @@ module.exports = {
             if (args[0] == 'set') {
                 const role = args[1];
 
-                console.log(role);
+                console.log(role.name);
             } else {
                 const invalidSyntax = new Discord.MessageEmbed()
                     .setColor('#ff0000')
