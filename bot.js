1df97e67429577cd2c7b2e504cd6d007efeb6125
Tetra <0xtetra420@protonmail.com>
2021-01-04 19:50:47 +0000

    autorole testing

--- bot.js
+++ bot.js
@@ -66,8 +66,7 @@ client.on('guildDelete', async (guild) => {
 
 // Give role upon member join
 client.on('guildMemberAdd', async (member) => {
-    console.log('member joined');
-    await database.getAutorole(message.guild).then(autorole => {
+    await database.getAutorole(member.guild).then(autorole => {
         if (!autorole) return;
         const role = member.guild.roles.cache.find(role => role.id === autorole);
         member.roles.add(role);
