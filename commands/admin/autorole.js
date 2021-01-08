6f82687263ec01d0be67f319ac22525b083f4356
0xTetra <0xtetra420@protonmail.com>
2021-01-08 16:12:28 +0000

    autorole shit

--- commands/admin/autorole.js
+++ commands/admin/autorole.js
@@ -38,7 +38,7 @@ module.exports = {
                     const autoroleSuccess = new Discord.MessageEmbed()
                         .setColor('#80ff33')
                         .setTitle('Guild Auto Role Changed')
-                        .setDescription(`You have successfully changed the auto role to <@&${role}>.`);
+                        .setDescription(`You have successfully changed the auto role to <@&${roleID}>.`);
     
                     return await message.channel.send(autoroleSuccess);
                 } else {
