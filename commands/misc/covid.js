0367f3e50f95d2aa8eaad6e1098265fc18e13774
0xTetra <0xtetra420@protonmail.com>
2021-02-01 20:08:23 +0000

    number formatting - covid

--- commands/misc/covid.js
+++ commands/misc/covid.js
@@ -6,13 +6,17 @@ module.exports = {
     description: 'Shows COVID-19 Stats globally, or for a specific country.',
     category: 'misc',
     run: async (client, message, args) => {
+        const formatNumber = async (x) => {
+            return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        }
+
         await axios.get('https://api.covid19api.com/summary').then(stats => {
-            const globalNewConfirmed = stats.data.Global.NewConfirmed;
-            const globalTotalConfirmed = stats.data.Global.TotalConfirmed;
-            const globalNewDeaths = stats.data.Global.NewDeaths;
-            const globalTotalDeaths = stats.data.Global.TotalDeaths;
-            const globalNewRecovered = stats.data.Global.NewRecovered;
-            const globalTotalRecovered = stats.data.Global.TotalRecovered;
+            const globalNewConfirmed = formatNumber(stats.data.Global.NewConfirmed);
+            const globalTotalConfirmed = formatNumber(stats.data.Global.TotalConfirmed);
+            const globalNewDeaths = formatNumber(stats.data.Global.NewDeaths);
+            const globalTotalDeaths = formatNumber(stats.data.Global.TotalDeaths);
+            const globalNewRecovered = formatNumber(stats.data.Global.NewRecovered);
+            const globalTotalRecovered = formatNumber(stats.data.Global.TotalRecovered);
 
             const statsEmbed = new Discord.MessageEmbed()
                 .setColor('#80ff33')
